eb46c2f0b99e8039faea4207c48101e21b531485
Sujan Shrestha <github@sujanshrestha.ca>
2026-08-28 16:11:03 +0000

    Refactor main function to improve error handling and streamline entry loading

--- main.go
+++ main.go
@@ -15,12 +15,15 @@ type Entry struct {
 }
 
 func main() {
+
 	configDir, err := os.UserConfigDir()
-	fmt.Println(configDir)
+	if err != nil {
+		fmt.Println("Error getting config directory:", err)
+		return
+	}
+
 	appDir := filepath.Join(configDir, "tm")
-	fmt.Println(appDir)
 	tasksFile := filepath.Join(appDir, "tasks.json")
-	fmt.Println(tasksFile)
 
 	err = os.MkdirAll(appDir, 0755)
 	if err != nil {
@@ -33,32 +36,29 @@ func main() {
 		return
 	}
 
-	entries := []Entry{}
+	if os.Args[1] == "--generate" {
 
-	file, err := os.Open(tasksFile)
+
+		fmt.Println("Generate command detected")
+		entries, err := loadEntries(tasksFile)
+		
+		if err != nil {
+			fmt.Println("Error loading entries:", err)
+			return
+		}
+		fmt.Println("Entries loaded:", entries)
+		return
+	}
+
+	entries, err := loadEntries(tasksFile)
 
 	if err != nil {
-		if os.IsNotExist(err) {
-			// this is okay: first run
-		} else {
-			fmt.Println("Error opening file:", err)
-			return
-		}
-	} else {
-		defer file.Close()
-
-		decoder := json.NewDecoder(file)
-		err = decoder.Decode(&entries)
-		if err != nil {
-			fmt.Println("Error decoding JSON:", err.Error())
-			file.Close()
-			return
-		}
-
+		fmt.Println("Error loading entries:", err)
+		return
 	}
 	entries = append(entries, Entry{Time: time.Now(), Message: strings.Join(os.Args[1:], " ")})
 
-	file, err = os.Create(tasksFile)
+	file, err := os.Create(tasksFile)
 	if err != nil {
 		fmt.Println("Error creating file:", err)
 		return
@@ -74,5 +74,30 @@ func main() {
 	}
 
 	fmt.Println(entries)
+
+}
+
+func loadEntries(tasksFile string) ([]Entry, error) {
+
+	entries := []Entry{}
+	file, err := os.Open(tasksFile)
+	if err != nil {
+		if os.IsNotExist(err) {
+			// this is okay: first run
+		} else {
+			return nil, fmt.Errorf("error opening file: %v", err)
+		}
+	} else {
+		defer file.Close()
+
+		decoder := json.NewDecoder(file)
+		err = decoder.Decode(&entries)
+		if err != nil {
+
+			return nil, fmt.Errorf("error decoding JSON: %v", err)
+		}
+
+	}
+	return entries, nil
 
 }
